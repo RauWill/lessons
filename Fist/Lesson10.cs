7e03d9e232edbd025fe516648389f0f2e0f6aa1f
Ravil <isxakovravil82@gmail.com>
2021-07-17 12:26:40 +0600

    Создан новый класс

---
+++ Fist/Lesson10.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fist
+{
+    class Lesson10
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+        }
+    }
+}
