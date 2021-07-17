6cfc073bd1909e97d3cbd4dff2c93571218a7a38
Ravil <isxakovravil82@gmail.com>
2021-07-17 12:08:06 +0600

    Изменены названия переменных и убраны пропуски

---
+++ Fist/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Fist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            int a, b, c, d;
+
+            Console.WriteLine("Введите первое число");
+
+            str = Console.ReadLine();
+
+            a = int.Parse(str);
+
+            Console.WriteLine("Введите второе число");
+
+            str = Console.ReadLine();
+
+            b = int.Parse(str);
+
+            c = a + b;
+
+            d = a * b;
+
+            Console.WriteLine("Сумма ваших чисел: " + c);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Произведение ваших чисел: " + d);  
+              
+
+
+
+            
+
+
+
+        }
+    }
+}
