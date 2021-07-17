28e28d0f2824c3775189b5b3b66c71126c4f9a7d
RauWill <87560214+RauWill@users.noreply.github.com>
2021-07-17 12:31:54 +0600

    Update Lesson10.cs

--- Fist/Lesson10.cs
+++ Fist/Lesson10.cs
@@ -11,6 +11,7 @@ namespace Fist
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world");
+            Console.WriteLine("Hello, world from github");
         }
     }
 }
