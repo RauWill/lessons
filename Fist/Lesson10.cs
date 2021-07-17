1f60120b8c2cdaec500e6a41dc91e998b47234b9
Ravil <isxakovravil82@gmail.com>
2021-07-17 14:10:10 +0600

    авзыоа

--- Fist/Lesson10.cs
+++ Fist/Lesson10.cs
@@ -18,7 +18,7 @@ namespace Fist
             Console.WriteLine("Спасибо");
             Console.WriteLine("Урок изучен");
             Console.WriteLine("Новая строка");
-            Console.WriteLine("Получилось");
+            Console.WriteLine("Получилось111");
         }
     }
 }
